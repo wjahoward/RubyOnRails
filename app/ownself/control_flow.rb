f9de429109fbc74ade1d16c9b90b5563d18187de
howardWebPageFeedback <howardwang42@gmail.com>
2018-09-21 14:47:04 +0800

    Creating control_flow statements

---
+++ app/ownself/control_flow.rb
@@ -0,0 +1,25 @@
+# control flow - if, elsif, else
+
+x = 5
+
+if x == 10
+    puts "x is 10"
+
+
+elsif x == 5
+    puts "x is 5"
+
+else
+    puts "x is something else"
+
+end
+
+# control flow - unless and alternative syntax
+
+y = 10
+unless y == 5 # unless is the opposite of 'if'
+    puts "y is not 5"
+
+else
+    puts "y is 5"
+end
