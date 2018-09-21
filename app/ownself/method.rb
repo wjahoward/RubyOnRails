4f4e9064c5eeb5ad4bc383af694968ebf61e4571
howardWebPageFeedback <howardwang42@gmail.com>
2018-09-21 15:20:51 +0800

    Try out methods

---
+++ app/ownself/method.rb
@@ -0,0 +1,10 @@
+def multiplyDefault(num1 = 6, num2 = 6) # you can initalize variables to having default values
+    puts "The result is #{num1 * num2}"
+end
+
+def multiply(num1, num2)
+    puts "The result is #{num1 * num2}"
+end
+
+multiplyDefault()
+multiply(6,7)
